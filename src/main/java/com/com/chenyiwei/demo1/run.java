aab3ff1fe16d0d2eaab1fa9f974bb7e75d142711
Chenyiwei <chenyiwei10@hotmail.com>
2022-05-05 18:59:05 +0800

    变更

---
+++ src/main/java/com/com/chenyiwei/demo1/run.java
@@ -0,0 +1,13 @@
+package com.chenyiwei.demo1;
+
+/**
+ * @author Chenyiwei
+ * @version 1.0
+ * @date 2022/5/5 18:56
+ */
+
+public class run {
+    public static void main(String[] args) {
+        System.out.println(("hrr"));
+    }
+}
